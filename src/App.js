d6944e4fabc7169dbf5f69d5c9cfda2d44663d52
Anand Jaiswar <anandjaiswar02@gmail.com>
2023-01-03 21:50:09 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ import LoadingBar from 'react-top-loading-bar'
 
 const App = () => {
   const pageSize = 5;
-  // const apiKey = process.env.REACT_APP_NEWS_API
+  const apiKey = process.env.REACT_APP_NEWS_API
   // const apiKey = "d093053d72bc40248998159804e0e67d"
-  const apiKey = "4dbc17e007ab436fb66416009dfb59a8"
+  // const apiKey = "4dbc17e007ab436fb66416009dfb59a8"
 
   const [progress, setProgress] = useState(0)
 
@@ -28,14 +28,14 @@ const App = () => {
         />
         <Routes>
 
-          <Route exact path="/" element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />} />
-          <Route exact path="/business" element={<News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business" />} />
-          <Route exact path="/entertainment" element={<News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment" />} />
-          <Route exact path="/general" element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />} />
-          <Route exact path="/health" element={<News setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="in" category="health" />} />
-          <Route exact path="/science" element={<News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science" />} />
-          <Route exact path="/sports" element={<News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports" />} />
-          <Route exact path="/technology" element={<News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology" />} />
+          <Route exact path="/NewsMonkey/" element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />} />
+          <Route exact path="/NewsMonkey/business" element={<News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business" />} />
+          <Route exact path="/NewsMonkey/entertainment" element={<News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment" />} />
+          <Route exact path="/NewsMonkey/general" element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />} />
+          <Route exact path="/NewsMonkey/health" element={<News setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="in" category="health" />} />
+          <Route exact path="/NewsMonkey/science" element={<News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science" />} />
+          <Route exact path="/NewsMonkey/sports" element={<News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports" />} />
+          <Route exact path="/NewsMonkey/technology" element={<News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology" />} />
 
 
         </Routes>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,20 +8,20 @@ const NavBar = () => {
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">NewsMonkey</Link>
+                    <Link className="navbar-brand" to="/NewsMonkey/">NewsMonkey</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item"><Link className="nav-link" aria-current="page" to="/">Home</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/Business">Business</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/Entertainment">Entertainment</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/General">General</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/Health">Health</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/Science">Science</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/Sports">Sports</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/Technology">Technology</Link></li>
+                            <li className="nav-item"><Link className="nav-link" aria-current="page" to="/NewsMonkey/">Home</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/NewsMonkey/Business">Business</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/NewsMonkey/Entertainment">Entertainment</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/NewsMonkey/General">General</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/NewsMonkey/Health">Health</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/NewsMonkey/Science">Science</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/NewsMonkey/Sports">Sports</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/NewsMonkey/Technology">Technology</Link></li>
                         </ul>
                     </div>
                 </div>
