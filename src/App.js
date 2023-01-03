1860e7a2a19fcdf6785cc8232451c95a42e0589c
Anand Jaiswar <anandjaiswar02@gmail.com>
2023-01-03 20:51:38 +0530

    New commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import './App.css';
 
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoadingBar from 'react-top-loading-bar'
 
-export default class App extends Component {
 
-  render() {
-    return (
-      <div>
-        <Navbar/>
-        <News/>
-      </div>
-    )
-  }
+const App = () => {
+  const pageSize = 5;
+  // const apiKey = process.env.REACT_APP_NEWS_API
+  // const apiKey = "d093053d72bc40248998159804e0e67d"
+  const apiKey = "4dbc17e007ab436fb66416009dfb59a8"
+
+  const [progress, setProgress] = useState(0)
+
+
+
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <LoadingBar
+          height={3}
+          color='#f11946'
+          progress={progress}
+        />
+        <Routes>
+
+          <Route exact path="/" element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />} />
+          <Route exact path="/business" element={<News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business" />} />
+          <Route exact path="/entertainment" element={<News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment" />} />
+          <Route exact path="/general" element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />} />
+          <Route exact path="/health" element={<News setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="in" category="health" />} />
+          <Route exact path="/science" element={<News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science" />} />
+          <Route exact path="/sports" element={<News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports" />} />
+          <Route exact path="/technology" element={<News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology" />} />
+
+
+        </Routes>
+      </Router>
+    </div>
+  )
 }
+
+export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,33 @@
 // import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default class Navbar extends Component {
-    // static propTypes = { second: third }
+const NavBar = () => {
 
-    render() {
-        return (
-            <div>
-                <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="/">NewsMonkey</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="/">Home</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="/about">About</a>
-                                </li>
-                                
-                                
-                            </ul>
-
-                        </div>
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">NewsMonkey</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item"><Link className="nav-link" aria-current="page" to="/">Home</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/Business">Business</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/Entertainment">Entertainment</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/General">General</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/Health">Health</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/Science">Science</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/Sports">Sports</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/Technology">Technology</Link></li>
+                        </ul>
                     </div>
-                </nav>
-            </div>
-        )
-    }
+                </div>
+            </nav>
+        </div>
+    )
 }
+
+export default NavBar
