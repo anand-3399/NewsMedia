6072f7733f00e20c0ce6f031ac2ead1fa22a6ba6
anand-3399 <anandjaiswar02@gmail.com>
2023-09-16 20:23:20 +0530

    Solved buffering timed out Error

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb://localhost:27017/newsmedia';
+const mongoURI = 'mongodb://127.0.0.1:27017/newsmedia';
 
 mongoose.set("strictQuery", false);
 const connectToMongo = () => {
